d050aa66d21fc9f4b558edc3ba7debf86a1cc879
Tony <tonytechnogym1@gmail.com>
2024-06-13 22:32:48 +0200

    Revert "commit of exercises"
    This reverts commit 33033249a7ce93aba7a07f65c1a1d44f421f35d8.

--- java-projects1/java-projects1/src/main/java/se/lexicon/part1/exercise1.java
+++
@@ -1,9 +0,0 @@
-package se.lexicon.part1;
-
-public class exercise1 {
-    public static void main(String[] args) {
-
-        System.out.println("Hello");
-        System.out.println("Tony");
-    }
-}
